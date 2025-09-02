1ee4b6769233db52e07195e7bb0e7c0a80c15f1b
vavengh <112110471+vavengh@users.noreply.github.com>
2025-09-02 15:44:53 +0000

    basic lamp with button

---
+++ config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+[
+  { 
+    name: "Button",
+    page: "button.html",
+    debug: "local",
+  },
+  { 
+    name: "Lamp",
+    page: "index.html",
+    main: true,
+    debug: "master",
+  }
+]);
